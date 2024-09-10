c28a5a89edc0d30ddb8049e723fc48138095601c
ccozens <ccozens@gmail.com>
2024-09-10 09:48:11 +0100

    added sqlitecloud drivers, instantiated db and wrote data fetching

--- src/api/route.ts
+++ src/api/route.ts
@@ -1,17 +1,40 @@
 // imports
-import { type NextRequest } from 'next/server'
+import { type NextRequest } from "next/server";
+import { Database } from "@sqlitecloud/drivers";
 
 // revalidate using ISR
-export const revalidate = 60
+export const revalidate = 60;
 
-// // set database to use
+// set database to use
+const database = new Database(process.env.SQLITECLOUD_CONNECTION_STRING);
 const setDatabase = async () => {
-	await database.sql`USE DATABASE svelte_lists.sqlite`;
+  await database.sql`USE DATABASE svelte_lists.sqlite`;
 };
 
 await setDatabase();
 
 // fetch data
-export async function GET(request: NextRequest) {
+export async function GET() {
+  // get data
+  const headings: Heading[] = await database.sql`SELECT * FROM headings`;
+  const tasks: TaskWithID[] =
+    await database.sql`SELECT todo_id, id, text, done FROM tasks`;
 
+  // join headings and tasks
+  const todos: Todo[] = headings.map((heading) => {
+    return {
+      heading: heading.heading,
+      tasks: tasks
+        .filter((task) => task.todo_id === heading.id)
+        .map((task) => ({
+          id: task.id,
+          text: task.text,
+          done: task.done,
+        })),
+    };
+  });
+
+  return {
+    todos,
+  };
 }
